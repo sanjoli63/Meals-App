8f8adcdae94d1bf388bf4f4cad99ac6fb99678d8
sanjoli63 <sanjoligo6mar@gmail.com>
2021-05-12 15:52:18 +0530

    category meal screen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -2,17 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/category_meals.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
   CategoryItem(
+    this.id,
     this.title,
     this.color,
   );
   void selectCategory(BuildContext ctx) {
-    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
-      return CategoryMealsScreen();
-    }));
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoryMealsScreen(id, title);
+        },
+      ),
+    );
   }
 
   @override
